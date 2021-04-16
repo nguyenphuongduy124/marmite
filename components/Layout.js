1f94987a04e3eec423b475c15bab6979d4f455bb
unknown <npduy124@gmail.com>
2021-04-16 18:52:23 +0700

    Init

--- components/Layout.js
+++ components/Layout.js
@@ -7,7 +7,7 @@ export default function Layout({ children }) {
         <Link href="/">
           <a>
             <h1>
-              <span>Just Add</span>
+              <span className="info">Just Add</span>
               <span>Marmite</span>
             </h1>
             <h2>Spread The Joy</h2>
--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,7 +1,56 @@
-export default function RecipeDetails() {
-  return (
-    <div>
-      Recipe Details
+import { createClient } from "contentful";
+import Image from 'next/image';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+const client = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+});
+export async function getStaticPaths() {
+  const res = await client.getEntries({
+    content_type: "recipe",
+  });
+  const paths = res.items.map(item => ({
+    params: { slug: item.fields.slug },
+  }));
+
+  return { paths, fallback: false };
+}
+
+export async function getStaticProps({ params }) {
+  const { items } = await client.getEntries({
+    content_type: "recipe",
+    "fields.slug": params.slug,
+  });
+
+  return {
+    props: { recipe: items[0] },
+  };
+}
+
+export default function RecipeDetails({recipe}) {
+  console.log(recipe);
+  return <div>
+    <div className="banner">
+      <Image src={`https:${recipe.fields.featuredImage.fields.file.url}`} 
+      width={recipe.fields.featuredImage.fields.file.details.image.width}
+      height={recipe.fields.featuredImage.fields.file.details.image.height}
+      />
     </div>
-  )
+    <div className="title">
+      <h4>{recipe.fields.title}</h4>
+    </div>
+    <div className="time">
+      <p>This marmite need {recipe.fields.cookingTime} to cook</p>
+    </div>
+    <div className="ingredients">
+      <ul>
+        {recipe.fields.ingredients.map(ing=>(
+          <li key={ing}>{ing}</li>
+        ))}
+      </ul>
+    </div>
+    <div className="method">
+      {documentToReactComponents(recipe.fields.method)}
+    </div>
+  </div>;
 }
